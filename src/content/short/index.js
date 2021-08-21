ed5dea31de137d88a194c0fc175c06da1af81e92
Felipe Gomes <contato@felipegomss.com.br>
2021-08-21 15:08:58 -0300

    error handler

--- src/content/short/index.js
+++ src/content/short/index.js
@@ -7,18 +7,28 @@ import './style.scss';
 const Short = () => {
 
 	const [urlSrt, setUrlSrt] = useState({
-		url: ''
+		url: '',
+		erro: ''
 	});
 	const [urlInfo, setfullUrl] = useState({
 		fullUrl: ''
 	})
 
+	const disallow = "shrtco.de/disallowed"
+
 	const handleSubmit = (e) => {
 		setUrlSrt({ url: 'Shortening...' })
 		api.get(`shorten?url=${urlInfo.fullUrl}`)
 			.then((response) => {
+				if (response.data.ok === true) {
+					console.log(response.data)
+					setUrlSrt({
+						url: response.data.result.short_link
+					})
+				}
+			}).catch((err) => {
 				setUrlSrt({
-					url: response.data.result.short_link
+					url: "The link you entered is a disallowed link, for more infos see"
 				})
 			})
 		e.preventDefault();
@@ -54,10 +64,18 @@ const Short = () => {
 					<div className="container show">
 						<span>
 							{urlSrt.url}
+							{urlSrt.url === 'The link you entered is a disallowed link, for more infos see' && (
+								<span>
+									&nbsp;
+									<a href={disallow}>
+										{disallow}
+									</a>
+								</span>
+							)}
 						</span>
 						{urlSrt.url !== 'Copied to clipboard' && (
 							<div>
-								{urlSrt.url !== 'Shortening...' && (
+								{urlSrt.url !== 'Shortening...' && urlSrt.url !== 'The link you entered is a disallowed link, for more infos see' && (
 									<CopyClipboard text={urlSrt.url}>
 										<button className='copy' onClick={clear}>Copy</button>
 									</CopyClipboard>
